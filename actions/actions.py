46a23dd9d1d6f4e4935b3bad4bab4fffae4136ef
Lakshmi-1212 <lakshmi1212@gmail.com>
2022-10-09 23:16:03 +0530

    Updated actions file to utter a consolidated message

--- actions/actions.py
+++ actions/actions.py
@@ -9,7 +9,7 @@ from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
 
 CRIC_API_URL = "https://api.cricapi.com/v1/"
-CRIC_API_KEY = "<UPDATE API KEY HERE>"
+CRIC_API_KEY = "<UPDATE_API_KEY_HERE>"
 
 SHOW_RECENT_MATCHES = 5
 
@@ -25,11 +25,12 @@ class ActionGetRecentMatches(Action):
 			matches_data = [x for x in matches_data if "matchType" in x]
 			recent_matches = matches_data[:SHOW_RECENT_MATCHES]
 			dispatcher.utter_message(f'Showing the status of {len(recent_matches)} recent matches\n')
+			msg = ''
 			for index, match in enumerate(recent_matches):
-				msg = f'{index+1}. {match["matchType"].upper()} match between ' \
-					  f'{match["teams"][0]} and {match["teams"][1]} played on {match["date"]}\n' \
-					  f'Match Status: {match["status"]}\n'
-				dispatcher.utter_message(msg)
+				msg = msg + f'{index+1}. {match["matchType"].upper()} match between ' \
+					  f'{match["teams"][0]} and {match["teams"][1]} \n' \
+					  f'Date: {match["date"]}, Match Status: {match["status"]}\n'
+			dispatcher.utter_message(msg)
 		else:
 			dispatcher.utter_message(f'Unable to fetch the recent matches details. Please try with some other query!!')
 		return []
@@ -52,10 +53,11 @@ class ActionGetUpcomingMatches(Action):
 				matches_data = [x for x in matches_data if team_name in x["teams"]]
 			if len(matches_data):
 				dispatcher.utter_message(f'Showing the upcoming matches ({len(matches_data)}) for team: {team_name}\n')
+				msg = ''
 				for index, match in enumerate(matches_data):
-					msg = f'{index+1}. {match["matchType"].upper() if "matchType" in match else "A"} match between {match["teams"][0]} and {match["teams"][1]}. ' \
+					msg = msg + f'{index+1}. {match["matchType"].upper() if "matchType" in match else "A"} match between {match["teams"][0]} and {match["teams"][1]}. ' \
 						f'Date: {match["date"]}, Venue: {match["venue"]}\n'
-					dispatcher.utter_message(msg)
+				dispatcher.utter_message(msg)
 			else:
 				dispatcher.utter_message(f'No upcoming matches for team: {team_name}\n')
 		else:
